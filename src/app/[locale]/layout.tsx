69a3dd13bf8d0552e36ecaf00a231a2170fa5cff
Didierson Amuri <didiamuri@gmail.com>
2025-08-24 05:16:42 +0100

    Rename footer with correct case

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "../globals.css";
-import Footer from "@/components/footer";
+import Footer from "@/components/Footer";
 import Header from "@/components/header";
 import { i18n } from "@/i18n/i18n.config";
 import { getDictionary } from "@/i18n/dictionary";
