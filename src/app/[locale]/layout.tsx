79891603a49cd1f075b234f0c0de06da2160966c
Didierson Amuri <didiamuri@gmail.com>
2025-08-24 05:10:00 +0100

    Fix file case for Footer

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "../globals.css";
-import Header from "@/components/header";
 import Footer from "@/components/footer";
+import Header from "@/components/header";
 import { i18n } from "@/i18n/i18n.config";
 import { getDictionary } from "@/i18n/dictionary";
 import { Translation } from "@/types/translation";
